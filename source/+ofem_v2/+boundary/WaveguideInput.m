158edd22c916024979f2fcd7263e6b12e30ca1b2
Myromax <hagelm@hsu-hh.de>
2024-05-16 01:05:07 +0200

    Small fixes

--- source/+ofem_v2/+boundary/WaveguideInput.m
+++ source/+ofem_v2/+boundary/WaveguideInput.m
@@ -12,16 +12,20 @@ classdef WaveguideInput < handle & ofem_v2.boundary.FixedBoundary
 		index;
 		name;
 
+		u_in;
+		x;
+
 		u;
 	end
 	
 	methods
-		function obj = WaveguideInput(value, name, mesh)
-			if size(value,1) ~= 3
-				error('ofem_v2:boundaries:WaveguideInput',...
-					'Waveguide Input has to be an array of vectors size(3,numEdges)');
-			end
-			obj.value = value;
+		function obj = WaveguideInput(u_in, x, name, mesh)
+% 			if size(u_in,1) ~= 2 || size(u_in,2) ~= 2
+% 				error('ofem_v2:boundaries:WaveguideInput',...
+% 					'Waveguide Input has to be an array of vectors size(2,numEdges)');
+% 			end
+			obj.u_in = u_in;
+			obj.x = x;
 
 			if ischar(name)
 				n = size(mesh.bd, 2);
@@ -58,19 +62,37 @@ classdef WaveguideInput < handle & ofem_v2.boundary.FixedBoundary
 			v1 = phys.geometry.co(:,:,phys.geometry.ed(obj.edges,1));
 			v2 = phys.geometry.co(:,:,phys.geometry.ed(obj.edges,2));
 			tang = v2-v1;
-			tang = tang*ofem_v2.tools.matrixarray(1/vecnorm(tang));
+			tang = double(tang);%*ofem_v2.tools.matrixarray(1/vecnorm(tang));
+
+			% Interpolate at edge center using stefans function
+			obj.cell2edge(phys);
 
 			b = ofem_v2.tools.matrixarray(zeros(size(eDofs)));
 			%obj.b = ofem_v2.tools.matrixarray(reshape(obj.b,1,size(eDofs,2),[]));
 
 			for q = 1:length(w)
-				b = b + (obj.value'*tang);
+				b = b + pagemtimes(obj.value,'transpose',tang,'none');
 			end
 
 			obj.u = sparse(eDofs(:),1,b(:),phys.DOFs.Nd,1);
 
 			u = obj.u;
 			phys.DOFs.reduceDOFs(obj);
+		end
+
+		function cell2edge(obj, phys)
+			% convert cell data from modes to edge data
+			v1 = phys.geometry.co(:,:,phys.geometry.ed(obj.edges,1));
+			v2 = phys.geometry.co(:,:,phys.geometry.ed(obj.edges,2));
+			xq = (v1+v2)/2;
+			xq = xq(1:2,:,:);
+			n = 5;		
+    		co_idx = knnsearch(obj.x',squeeze(xq)','K',n)';
+		
+    		ui_x = (sum((reshape(obj.u_in(1,co_idx(:,:)'),[],n)),2)/n);
+    		ui_y = (sum((reshape(obj.u_in(2,co_idx(:,:)'),[],n)),2)/n);
+			obj.value = reshape([ui_x,ui_y]',2,1,[]);
+			obj.value(3,:,:) = 0;
 		end
 	end
 end
